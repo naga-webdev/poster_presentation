725820de18060ce58513fd4d6be6b8e0afe81979
successnaga <naga2054@gmail.com>
2017-09-24 09:19:45 +0530

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('port',( process.env.PORT || 5000 ))
+
+app.use(express.static(__dirname +'/dist'));
+
+
+app.get('/*',function(req,res){
+    res.sendFile('index.html', path.join(__dirname+'/dist'));
+})
+
+
+app.listen(app.get('port') ,function(){
+    console.log('server is running at '+ app.get('port'));
+})
